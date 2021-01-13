c455cabe4819da426e46fc8970519a8dbb4efbc5
flobev <florian.bevierre@gmail.com>
2021-01-13 23:28:55 +0100

    Ajout des Interfaces Dao Airbabouche

---
+++ src/main/java/dao/PaysDao.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import com.airbabouche.principal.Pays;
+/**
+ *
+ * @author flo
+ */
+public interface PaysDao {
+    Pays créer(); 
+}
